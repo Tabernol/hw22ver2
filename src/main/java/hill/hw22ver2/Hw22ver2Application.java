e7129c42070fd332c0ab7811ca12f7ce1f4ca494
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-06-16 15:00:38 +0300

    hw22start

---
+++ src/main/java/hill/hw22ver2/Hw22ver2Application.java
@@ -0,0 +1,13 @@
+package hill.hw22ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw22ver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw22ver2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hill/hw22ver2/Hw22ver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hill.hw22ver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw22ver2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
